9db5a395d633ef6084eef54dffe42310489d0afb
Shubham Patil <sbp007@ymail.com>
2024-09-20 01:12:45 -0400

    Updates

---
+++ _next/static/chunks/app/layout-b537b357d100588b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5508:function(n,e,_){Promise.resolve().then(_.t.bind(_,7989,23))},7989:function(n){n.exports={style:{fontFamily:"'__Inter_36bd41', '__Inter_Fallback_36bd41'",fontStyle:"normal"},className:"__className_36bd41"}}},function(n){n.O(0,[575,971,23,744],function(){return n(n.s=5508)}),_N_E=n.O()}]);
